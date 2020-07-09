5606fea90344bff28541dfa86497baf9376ae961
imoghul <moghul02@gmail.com>
2020-07-09 15:25:03 -0400

    added ability to input function

--- Euler.py
+++ Euler.py
@@ -1,4 +1,14 @@
+import sys
+
+sys.path.insert(1,'/Calculus3')
+
+from Calculus3.CalculusOperator import CalculusOperator
+
+cOperator = CalculusOperator()
+
 ####### declare constants here
+func = str(raw_input("Please input the function: "))
+
 xInitial= int(input("please type the initial x value: "))
 
 yInitial= int(input("please type the initial y value: "))
@@ -15,13 +25,13 @@ n=endVal
 def x(n): #### returns x sub n
   return xInitial+(n*delta)
 def y(n): #### returns y sub n
-  print (n);
+  #print (n);
   if(n<=0):
     return yInitial
   return y(n-1)+delta*(get(x(n-1),y(n-1)))
   
 def get(x,y): ##### define funcition here
-  return (x*x)/y
+  return cOperator.plug(func,[ ["x",x] , ["y",y] ])
 
-print('y at value: ')
-print (y((endVal-yInitial)/delta))
+print('y at value: ',y((endVal-yInitial)/delta))
+
