38d651c683723eb3a74ee73c0e68c3c3c0d9b0a9
imoghul <moghul02@gmail.com>
2020-07-11 23:54:48 -0400

    fixed bug?

--- Euler.py
+++ Euler.py
@@ -1,4 +1,10 @@
 import sys
+import os
+
+if(str(raw_input("Would you like to update the module? (answer yes if so) "))=="yes"):
+  os.system('cd calculus3')
+  os.system('git pull')
+  os.system('cd ..')
 
 sys.path.insert(1,'/Calculus3')
 
@@ -9,13 +15,13 @@ cOperator = CalculusOperator()
 ####### declare constants here
 func = str(raw_input("Please input the function: "))
 
-xInitial= int(input("please type the initial x value: "))
+xInitial= float(input("please type the initial x value: "))
 
-yInitial= int(input("please type the initial y value: "))
+yInitial= float(input("please type the initial y value: "))
 
 delta= float(input("please type the delta x value: "))
 
-endVal= int(input("please type the value to evaluate at: "))
+endVal= float(input("please type the value to evaluate at: "))
 
 while(endVal<=xInitial):
   endVal=input("please type a value bigger than x Initial: ")
@@ -25,13 +31,17 @@ n=endVal
 def x(n): #### returns x sub n
   return xInitial+(n*delta)
 def y(n): #### returns y sub n
-  #print (n);
+
   if(n<=0):
+
     return yInitial
+
   return y(n-1)+delta*(get(x(n-1),y(n-1)))
   
 def get(x,y): ##### define funcition here
   return cOperator.plug(func,[ ["x",x] , ["y",y] ])
 
-print('y at value: ',y((endVal-yInitial)/delta))
+
+print('y at value: ')
+print y((endVal-xInitial)/delta)
 
